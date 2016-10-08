becc266c3d5e34f49c6efeb4397ba59c6868d8f4
fcu-d0323500 <wankwunkit@gmail.com>
2016-10-09 02:45:26 +0800

    newest and debug and shorter code

---
+++ KeyboardScanner.java
@@ -0,0 +1,28 @@
+package hw;
+
+import java.util.Scanner;
+
+public class KeyboardScanner {
+
+	
+	public static void main(String[] args) {
+		
+		int a ;
+		float b;
+		String c;
+		float d;
+			
+		Scanner Scanner = new Scanner(System.in);
+		
+		System.out.println("Enter a integer:");
+		a = Scanner.nextInt();
+		System.out.println("Enter a float point number:");
+		b = Scanner.nextFloat();
+		System.out.println("Enter your name:");
+		c = Scanner.next();
+		
+		d = a*b ;
+		System.out.printf("Hi "+ c +", the mutiplication of " + a + " and " + b + " is " + "%E" , d);
+	
+	}
+}
